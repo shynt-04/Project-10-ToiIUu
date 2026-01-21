3ccf3509f62596507d66f9df13a0a01933ba03b2
Nguyen Dang Phuc <146701277+phucnguyendang@users.noreply.github.com>
2026-01-21 22:08:08 +0700

    Refactor greedy function and improve stock checking

--- heuristic/greedy.cpp
+++ heuristic/greedy.cpp
@@ -4,10 +4,6 @@
 #include <cstring>
 #include <cmath>
 
-#define MOD 1000000007
-#define Task "annotshy"
-#define F first
-#define S second
 #define int long long
 
 using namespace std;
@@ -18,9 +14,18 @@ int n, m;
 int Q[maxn][maxn];
 int d[maxn][maxn];
 int q[maxn], cur_q[maxn];
-int best_ans = 1e18, time_cnt = 0;
 int vis[maxn];
 vector<int> ans;
+
+// kiểm tra xem kệ 'shelf_idx' có món hàng nào mình đang thiếu không
+bool is_useful(int shelf_idx) {
+    for (int i = 1; i <= n; ++i) {
+        if (cur_q[i] < q[i] && Q[i][shelf_idx] > 0) {
+            return true; 
+        }
+    }
+    return false;
+}
 
 bool check_stock() {
     for (int i = 1; i <= n; ++ i) {
@@ -29,57 +34,59 @@ bool check_stock() {
     return 1;
 }
 
-void greedy() {
+void greedy_smart_nn() {
     int start = 0, total_dis = 0;
+    
     while (!check_stock()) {
         int best = -1;
+        
+        // Tìm kệ gần nhất có ích
         for (int i = 1; i <= m; ++ i) {
             if (vis[i]) continue;
+            if (!is_useful(i)) continue; 
             if (best == -1 || d[start][i] < d[start][best]) {
                 best = i;
             }
         }
+        
+        if (best == -1) break; // Không còn đường đi
+
         vis[best] = 1;
         total_dis += d[start][best];
+        
+        // Cập nhật kho hàng
         for (int i = 1; i <= n; ++ i) {
-            cur_q[i] += Q[i][best];
+            cur_q[i] += Q[i][best]; 
         }
+        
         start = best;
         ans.push_back(best);
     }
+    
     total_dis += d[start][0];
+    
     cout << ans.size() << '\n';
     for (auto x : ans) cout << x << ' ';
 }
 
-void solve(int Test) {
+void solve() {
     cin >> n >> m;
     for (int i = 1; i <= n; ++ i) {
-        for (int j = 1; j <= m; ++ j) {
-            cin >> Q[i][j];
-        }
+        for (int j = 1; j <= m; ++ j) cin >> Q[i][j];
     }
     for (int i = 0; i <= m; ++ i) {
-        for (int j = 0; j <= m; ++ j) {
-            cin >> d[i][j];
-        }
+        for (int j = 0; j <= m; ++ j) cin >> d[i][j];
     }
     for (int i = 1; i <= n; ++ i) cin >> q[i];
-    greedy();
+    
+    greedy_smart_nn();
 }
 
 signed main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0), cout.tie(0);
-    if (fopen(Task".inp", "r")) {
-        freopen(Task".inp", "r", stdin);
-        freopen(Task".out", "w", stdout);
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    if (fopen("annotshy.inp", "r")) {
+        freopen("annotshy.inp", "r", stdin);
+        freopen("annotshy.out", "w", stdout);
     }
-    int test = 1;
-    // cin >> test;
-    for (int i = 1; i <= test; ++ i) {
-        // cout << "Case #" << i << ": ";
-        solve(i);
-    }
+    solve();
 }
-// g++ .\annotshy.cpp -o annotshy.exe -Wall -Wextra -std=c++17
